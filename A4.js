12283b47f197b76f9e9039bff8d686bbbcd81aa0
first--github-project <amd142457@gmail.com>
2026-03-08 01:58:24 +0300

    2nd commit

--- A4.js
+++ A4.js
@@ -155,7 +155,7 @@ function showCards(details) {
 
   details.forEach((detail) => {
     const card = document.createElement("div");
-    card.className = `w-11/12 mx-auto space-y-3 p-5 rounded-sm shadow-sm bg-white gap-4  ${detail.status === "open" ? "border-t-4 border-t-green-500" : "border-t-4 border-t-red-500"}`;
+    card.className = ` space-y-3 p-5 rounded-sm shadow-sm bg-white gap-4  ${detail.status === "open" ? "border-t-4 border-t-green-500" : "border-t-4 border-t-red-500"}`;
     card.innerHTML = `
     <div class="flex justify-between ">
          <img src="${detail.status === "open" ? "assets/Open-Status.png" : "assets/Closed- Status .png"}"  />
@@ -175,9 +175,46 @@ function showCards(details) {
         </div>
     
     `;
+    card.addEventListener("click", function () {
+      lodeWordDetails(detail.id);
+    });
     allCard.append(card);
   });
 }
+
+const lodeWordDetails = async (id) => {
+  const url = `https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`;
+  const res = await fetch(url);
+  const details = await res.json();
+  displyWorddetails(details.data);
+};
+
+const displyWorddetails = (text) => {
+  const datailscontainer = document.getElementById("datails-container");
+
+  datailscontainer.innerHTML = `
+  <div class="flex justify-between ">
+         <img src="${text.status === "open" ? "assets/Open-Status.png" : "assets/Closed- Status .png"}"  />
+    
+          <button class="btn rounded-full">${text.priority}</button>
+        </div>
+       <h2 class="text-xl font-bold">${text.title}</h2>
+       <p class="line-clamp-2">${text.description}</p>
+         <div class="flex gap-4">
+            <button class="btn rounded-2xl bg-yellow-100 border-yellow-600">Bug</button>
+            <button class="btn rounded-2xl bg-red-200 border-red-950">help wanted</button>
+        </div>
+    <div class="border-t-1 border-t-gray-500">
+          <p> #${text.author}</p>
+          <p>${text.createdAt}</p>
+        </div>
+        </div>
+              
+    
+    `;
+
+  document.getElementById("my_modal_5").showModal();
+};
 async function selectcatagory() {
   const allBtn = document.querySelectorAll("#button button");
   allBtn.forEach((btn) => {
